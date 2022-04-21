7fc7ade7f62eec3cf2dac3288ac7a64777e12e2a
GigolNft <GigolNft@gmail.com>
2022-04-22 01:25:18 +0200

    Set all types

--- src/funny.methods.js
+++ src/funny.methods.js
@@ -1,7 +1,27 @@
 var funnyMethods = {
 	// Method to test the variable is empty
 	isEmpty: function(variable, callback) {
+		if (typeof variable === "undefined") {
+			callback(true);
+			return;
+		}
+
+		if (variable === null) {
+			callback(true);
+			return;
+		}
+
 		if (variable == "") {
+			callback(true);
+			return;
+		}
+
+		if (variable == 0) {
+			callback(true);
+			return;
+		}
+
+		if (variable === false) {
 			callback(true);
 			return;
 		}
