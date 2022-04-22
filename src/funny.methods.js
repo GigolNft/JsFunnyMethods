8f0b43e332907150b2d4a587cfd111f6f27afc7c
GigolNft <GigolNft@gmail.com>
2022-04-22 02:13:59 +0200

    Refacto / Method isNotEmpty

--- src/funny.methods.js
+++ src/funny.methods.js
@@ -2,30 +2,60 @@ var funnyMethods = {
 	// Method to test the variable is empty
 	isEmpty: function(variable, callback) {
 		if (typeof variable === "undefined") {
-			callback(true);
-			return;
+			if (typeof callback === "function") {
+				callback(true);
+			}
+			return true;
 		}
 
 		if (variable === null) {
-			callback(true);
-			return;
+			if (typeof callback === "function") {
+				callback(true);
+			}
+			return true;
 		}
 
 		if (variable == "") {
-			callback(true);
-			return;
+			if (typeof callback === "function") {
+				callback(true);
+			}
+			return true;
 		}
 
 		if (variable == 0) {
-			callback(true);
-			return;
+			if (typeof callback === "function") {
+				callback(true);
+			}
+			return true;
 		}
 
 		if (variable === false) {
-			callback(true);
-			return;
+			if (typeof callback === "function") {
+				callback(true);
+			}
+			return true;
 		}
 
-		callback(false);
+		if (typeof callback === "function") {
+			callback(false);
+		}
+		return false;
+	},
+
+	// Method to test the variable is not empty
+	isNotEmpty: function(variable, callback) {
+		this.isEmpty(variable, function(empty) {
+			if (empty) {
+				if (typeof callback === "function") {
+					callback(false);
+				}
+				return false;
+			} else {
+				if (typeof callback === "function") {
+					callback(true);
+				}
+				return true;
+			}
+		});
 	}
 };
